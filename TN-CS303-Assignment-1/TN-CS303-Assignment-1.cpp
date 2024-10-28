599585165a3f2bd344b508a389b9c70ff2131cfa
Trevor Norton <nortrevor02@gmail.com>
2024-10-28 11:56:13 -0500

    Created Solution

---
+++ TN-CS303-Assignment-1/TN-CS303-Assignment-1.cpp
@@ -0,0 +1,9 @@
+// TN-CS303-Assignment-1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
