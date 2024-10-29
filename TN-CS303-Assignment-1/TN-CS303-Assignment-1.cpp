b1a3d9384a2088899f570e95480df06fece7a084
Trevor Norton <nortrevor02@gmail.com>
2024-10-29 00:58:29 -0500

    Fixed Main File

--- TN-CS303-Assignment-1/TN-CS303-Assignment-1.cpp
+++
@@ -1,9 +0,0 @@
-// TN-CS303-Assignment-1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
